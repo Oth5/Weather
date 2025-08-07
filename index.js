281a90e77f934ccd82347dd5b0db2bd4a5440c90
Oth5 <aaoo12aaoo1@gmail.com>
2025-08-07 17:40:01 +0300

    fix: add view engine setting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 3000;
 app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
