6dab287ad31720b266cc2f99d2cf2710bdf8fec2
Oth5 <aaoo12aaoo1@gmail.com>
2025-08-07 17:34:48 +0300

    c

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.set("views", "./views");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
